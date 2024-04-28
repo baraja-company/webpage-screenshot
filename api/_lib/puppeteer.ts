69024018aba3062d695e67aebb8420041a0a6af6
Jan Barášek <janbarasek@gmail.com>
2024-04-28 15:44:23 +0200

    Maintenance

--- api/_lib/puppeteer.ts
+++ api/_lib/puppeteer.ts
@@ -1,23 +1,18 @@
-import { launch, Page } from "puppeteer-core";
+import { launch } from "puppeteer-core";
 import chrome from "chrome-aws-lambda";
-let _page: Page | null;
 
-async function getPage() {
-  if (_page) return _page;
-  const options = {
+export async function getScreenshot(url, width, height) {
+  const browser = await launch({
     args: chrome.args,
     executablePath: await chrome.executablePath,
     headless: chrome.headless,
-  };
-  const browser = await launch(options);
-  _page = await browser.newPage();
-  return _page;
-}
-
-export async function getScreenshot(url, width, height) {
-  const page = await getPage();
+  });
+  const page = await browser.newPage();
   await page.goto(url);
-  await page.setViewport({ width: Number(width) || 1280, height: Number(height) || 720, deviceScaleFactor: 2 });
-  const file = await page.screenshot();
-  return file;
+  await page.setViewport({
+    width: Number(width) || 1280,
+    height: Number(height) || 720,
+    deviceScaleFactor: 2,
+  });
+  return await page.screenshot();
 }
